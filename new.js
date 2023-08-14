857fa19d608d667ac139d2e84d2bcb0b0f8b8bd1
shreedharvk <142148791+shreedharvk@users.noreply.github.com>
2023-08-14 14:45:29 +0530

    done with getElementbyid and class

---
+++ new.js
@@ -0,0 +1,11 @@
+var items=document.getElementsByClassName('list-group-item');
+console.log(items);
+
+console.log(items[1]);
+items[1].textContent = 'hello';
+
+var items=document.getElementsByTagName('li');
+console.log(items);
+
+console.log(items[2]);
+items[2].textContent = 'hello ooo';
